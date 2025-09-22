cd343cb576ada3f8fced402963c82de9fb78ec7d
Pieter van Ginkel <pvginkel@gmail.com>
2025-09-22 14:55:11 +0200

    Minor improvements.

--- claudebox.sh
+++ claudebox.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(dirname "$0")"
 
-if "$1" = ""; then
+if [ "$1" = "" ]; then
     $SCRIPT_DIR/llmbox.sh /usr/bin/thunk.sh claude --dangerously-skip-permissions
 else
     $SCRIPT_DIR/llmbox.sh $*

7850e6e6f1366f054eee0de6706296806de3f3c7
Pieter van Ginkel <pvginkel@gmail.com>
2025-09-22 21:09:52 +0200

    Minor improvements.

--- codex.sh
+++ codex.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(dirname "$0")"
 
 if [ "$1" = "" ]; then
-    $SCRIPT_DIR/llmbox.sh /usr/bin/thunk.sh codex --model gpt-5-codex
+    $SCRIPT_DIR/llmbox.sh /usr/bin/thunk.sh codex --model gpt-5-codex --dangerously-bypass-approvals-and-sandbox
 else
     $SCRIPT_DIR/llmbox.sh $*
 fi

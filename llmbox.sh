753e901e1e994f6ceed4a2e96310778e939f2ba4
Pieter van Ginkel <pvginkel@gmail.com>
2025-09-24 21:50:12 +0200

    Moved building the Docker image into DockerImages.

--- llmbox.sh
+++ llmbox.sh
@@ -14,29 +14,4 @@ cd "$(dirname "$0")"
 
 export UID=$(id -u) GID=$(id -g)
 
-if [ "$1" = "build" ]; then
-    docker compose build
-elif [ "$1" = "rebuild" ]; then
-    docker compose build --no-cache
-else
-    # Check if container is more than 1 day old and rebuild if needed
-    CONTAINER_AGE=$(docker inspect -f '{{.Created}}' llmbox-llmbox:latest 2>/dev/null | xargs -I{} date -d {} +%s 2>/dev/null)
-    CURRENT_TIME=$(date +%s)
-    if [ -n "$CONTAINER_AGE" ]; then
-        AGE_DIFF=$((CURRENT_TIME - CONTAINER_AGE))
-        if [ $AGE_DIFF -gt 86400 ]; then
-            cat <<EOF
-  ____  _____ ____  _   _ ___ _     ____ ___ _   _  ____
- |  _ \| ____| __ )| | | |_ _| |   |  _ \_ _| \ | |/ ___|
- | |_) |  _| |  _ \| | | || || |   | | | | ||  \| | |  _
- |  _ <| |___| |_) | |_| || || |___| |_| | || |\  | |_| |_ _ _
- |_| \_\_____|____/ \___/|___|_____|____/___|_| \_|\____(_|_|_)
-
-EOF
-
-            docker compose build --no-cache
-        fi
-    fi
-
-    docker compose run --rm -w $CONTAINER_PATH llmbox $*
-fi
+docker compose run --pull=always --rm -w $CONTAINER_PATH llmbox $*
--- thunk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if output="$(poetry env activate 2>/dev/null)"; then
-    eval "$output"
-fi
-
-$*
